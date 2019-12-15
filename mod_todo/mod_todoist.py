50acde6d71315e474066db392b8683cfc0e4f79e
Marc Urben <aegnor@mittelerde.ch>
2019-12-15 20:14:18 +0100

    if no api key is configure, don't fail

--- mod_todo/mod_todoist.py
+++ mod_todo/mod_todoist.py
@@ -5,25 +5,30 @@ import logging
 class ToDo:
     def __init__(self, opts):
         logging.debug("Todo API: TODOIST")
-        self.api = todoist.TodoistAPI(opts['api_key'])
-        self.api.sync()
+        self.api = False
+        if not opts['api_key']:
+            logging.warning("Not loading Todo API, since no api key is configured")
+        else:
+            self.api = todoist.TodoistAPI(opts['api_key'])
+            self.api.sync()
 
     def list(self):
         items = []
         # Loop through original array from Todoist and pull out 
         # items of interest
-        for item in self.api.state['items']:
-            if item['checked'] == 0:
-                items.append({
-                    "content": item['content'],
-                    "priority": item['priority'],
-                })
+        if self.api:
+            for item in self.api.state['items']:
+                if item['checked'] == 0:
+                    items.append({
+                        "content": item['content'],
+                        "priority": item['priority'],
+                    })
 
-        # Sort the array by priority    
-        items = sorted(items, key = lambda i: i['priority'])
+            # Sort the array by priority
+            items = sorted(items, key = lambda i: i['priority'])
 
-        # Reverse list, since Todoist sets priority in reverse.
-        # On web interface HIGH=Priority1, but stored in API as 4. who knows?!
-        items.reverse()
+            # Reverse list, since Todoist sets priority in reverse.
+            # On web interface HIGH=Priority1, but stored in API as 4. who knows?!
+            items.reverse()
 
         return items
