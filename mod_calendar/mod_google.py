9ffd9a713986b4e72dbab8713f6f05e25ad93ed0
Marc Urben <aegnor@mittelerde.ch>
2019-12-16 23:59:52 +0100

    implementing multiple google calendar support

--- mod_calendar/mod_google.py
+++ mod_calendar/mod_google.py
@@ -13,22 +13,45 @@ class Cal:
         ga = mod_google_auth.GoogleAuth()
         self.creds = ga.login()
         self.timeformat = options["timeformat"]
+        self.additional = options["additional"]
 
     def list(self):
+        calendar_ids = []
+        events = {}
+        items = []
+
         service = build('calendar', 'v3', credentials=self.creds)
-
         now = dt.utcnow().isoformat() + 'Z'
-        result = service.events().list(calendarId='primary', timeMin=now,
-                                       maxResults=20,
-                                       singleEvents=True,
-                                       orderBy='startTime').execute()
 
-        events = result.get('items', [])
+        page_token = None
+        while True:
+            calendar_list = service.calendarList().list(pageToken=page_token).execute()
+            for calendar_list_entry in calendar_list['items']:
+                if "primary" in calendar_list_entry.keys():
+                    if calendar_list_entry['primary']:
+                        calendar_ids.append(calendar_list_entry['id'])
+                    elif calendar_list_entry['summary'] == self.additional:
+                        calendar_ids.append(calendar_list_entry['id'])
+            page_token = calendar_list.get('nextPageToken')
+            if not page_token:
+                break
+
+        for id in calendar_ids:
+            result = service.events().list(calendarId=id, timeMin=now,
+                                           maxResults=10,
+                                           singleEvents=True,
+                                           orderBy='startTime').execute()
+
+            for event in result.get('items', []):
+                start = event['start'].get('dateTime', event['start'].get('date'))
+                if start in events.keys():
+                    start = "%sa" % start
+                events[start] = event
 
         # 2019-11-05T10:00:00-08:00
-        items = []
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
+
+        for event_key in sorted(events.keys()):
+            start = events[event_key]['start'].get('dateTime', events[event_key]['start'].get('date'))
 
             # Sunrise and Sunset.
             if self.timeformat == "12h":
@@ -41,7 +64,7 @@ class Cal:
             items.append({
                 "date": st_date,
                 "time": st_time,
-                "content": event['summary']
+                "content": events[event_key]['summary']
             })
 
         return items
