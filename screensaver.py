549385cbc2b5f0c624c0bcf1e1645c9c292b7fc6
Marc Urben <aegnor@mittelerde.ch>
2020-01-30 22:53:53 +0100

    adding screensaver script

--- screensaver.py
+++ screensaver.py
@@ -3,22 +3,24 @@
 import logging
 import os
 from driver import epd7in5b
+from PIL import Image
 
 # Setup Logging -  change to logging.DEBUG if you are having issues.
 logging.basicConfig(level=logging.DEBUG)
-logging.info("Screensager starting")
+logging.info("Screen saver starting")
 
 
 def main():
     epd = epd7in5b.EPD()
     epd.init()
-    logging.info("Display red")
-    epd.display_frame(epd.get_frame_buffer(os.path.join("resources", "red.png")))
-    logging.info("Display black")
-    epd.display_frame(epd.get_frame_buffer(os.path.join("resources", "black.png")))
-    logging.info("Display white")
-    epd.display_frame(epd.get_frame_buffer(os.path.join("resources", "white.png")))
-    logging.info("Screensaver finished")
+
+    images = ["red.png", "black.png", "white.png"]
+    for image in images:
+        logging.info("Display %s" % image)
+        image_data = Image.open(os.path.join("resources", image))
+        epd.display_frame(epd.get_frame_buffer(image_data))
+
+    logging.info("Screen saver finished")
 
 
 if __name__ == '__main__':
